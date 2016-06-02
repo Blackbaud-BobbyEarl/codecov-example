75895fdf92d2e301aec9563b14125b728095fd79
Bobby Earl <bobby.earl@blackbaud.com>
2016-06-02 13:52:34 -0400

    Adding token... oh well

--- lib/calc.ts
+++ lib/calc.ts
@@ -2,4 +2,7 @@ export default class Calculator {
     add(x : number, y : number) : number {
         return x + y;
     }
+    sub(x : number, y : number) : number {
+        return x - y;
+    }
 }
